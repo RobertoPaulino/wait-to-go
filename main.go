bd744a78181cfb9e0d696cbcc200ba711d4426b4
Robert <rpaulinodev@gmail.com>
2025-04-25 19:51:30 -0400

    Implementing Postgres DB

--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
+	"slices"
 	"sort"
 	"time"
+
+	_ "github.com/lib/pq"
 )
 
 type Entry struct {
 	ID          int
+	Position    int
 	FirstName   string
 	LastName    string
 	Email       string
@@ -28,7 +34,7 @@ func (q ByJoinTime) Len() int           { return len(q) }
 func (q ByJoinTime) Less(i, j int) bool { return q[i].JoinTime.Before(q[j].JoinTime) }
 func (q ByJoinTime) Swap(i, j int)      { q[i], q[j] = q[j], q[i] }
 
-func getTests() []Entry {
+func getTests() ([]Entry, []Entry) {
 	queue := []Entry{
 		{
 			ID:          1,
@@ -75,63 +81,98 @@ func getTests() []Entry {
 			Status:      StatusWaiting,
 			JoinTime:    time.Date(2025, 4, 20, 9, 2, 0, 0, time.UTC),
 		}}
-	return queue
+
+	sort.Sort(ByJoinTime(queue))
+	return queue, []Entry{}
 }
 
-func notifyNext(queue *[]Entry) {
-	sort.Sort(ByJoinTime(*queue))
+func notifyNext(queue *[]Entry, history *[]Entry) {
 
-	for index, value := range *queue {
-		if value.Status == StatusWaiting {
-			fmt.Printf("Now Notifying: %v \n", value.FirstName)
-			(*queue)[index].Status = StatusNotified
-			break
+	if (*queue)[0].Status == StatusWaiting {
+		fmt.Printf("Now Notifying: %v \n", (*queue)[0].FirstName)
+		(*queue)[0].Status = StatusNotified
+		*history = append(*history, (*queue)[0])
+		*queue = slices.Delete(*queue, 0, 1)
+
+		// Debug output
+		for index, value := range *queue {
+			if value.Status == StatusWaiting {
+				fmt.Printf("Position: %v Name:%v Status:%v \n", index, value.FirstName, value.Status)
+			}
 		}
+
+		fmt.Print("\n\n\n----------------------------------\n\n\n")
 	}
 
-	// Debug output
-	for index, value := range *queue {
-		if value.Status == StatusWaiting {
-			fmt.Printf("Position: %v Name:%v Status:%v \n", index, value.FirstName, value.Status)
-		}
-	}
-
-	fmt.Print("\n\n\n----------------------------------\n\n\n")
 }
 
 func addEntry(entry Entry, queue *[]Entry) {
-	*queue = append(*queue, entry)
+
+	if entry.Status == StatusWaiting {
+		*queue = append(*queue, entry)
+		sort.Sort(ByJoinTime(*queue))
+	} else {
+		fmt.Println("Tried adding entry with wrong status")
+	}
+}
+
+func createEntryTable(db *sql.DB) {
+
+	query := `CREATE TABLE IF NOT EXISTS entry (
+		id SERIAL PRIMARY KEY,
+		firstName VARCHAR(30) NOT NULL,
+		lastName VARCHAR(30) NOT NULL,
+		email VARCHAR(50),
+		phoneNumber VARCHAR(10),
+		status VARCHAR(20) NOT NULL,
+		joinTime timestamp DEFAULT NOW()
+	)`
+
+	_, err := db.Exec(query)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func insertEntry(db *sql.DB, entry Entry) int {
+	query := `INSERT INTO entry (firstName, lastName, email, phoneNumber, status, joinTime) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id`
+
+	var pk int
+	err := db.QueryRow(query, entry.FirstName, entry.LastName, entry.Email, entry.PhoneNumber, entry.Status, entry.JoinTime).Scan(&pk)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return pk
 }
 
 func main() {
-	testEntries := getTests()
 
-	newEntry := Entry{
-		ID:          10,
-		FirstName:   "John",
-		LastName:    "Doe",
-		Email:       "JDoe@ABC.net",
-		PhoneNumber: "111-111-1111",
-		Status:      StatusWaiting,
-		JoinTime:    time.Now(),
+	entryList, _ := getTests()
+	connStr := "postgres://postgres:sicreto@localhost:5432/gopgtest?sslmode=disable"
+
+	db, err := sql.Open("postgres", connStr)
+
+	defer db.Close()
+
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	notifyNext(&testEntries)
-	time.Sleep(3 * time.Second)
+	if err = db.Ping(); err != nil {
+		log.Fatal(err)
+	}
 
-	addEntry(newEntry, &testEntries)
-	time.Sleep(3 * time.Second)
+	createEntryTable(db)
 
-	notifyNext(&testEntries)
-	time.Sleep(3 * time.Second)
+	pk := insertEntry(db, entryList[0])
 
-	notifyNext(&testEntries)
-	time.Sleep(3 * time.Second)
-
-	notifyNext(&testEntries)
-	time.Sleep(3 * time.Second)
-
-	notifyNext(&testEntries)
-	time.Sleep(3 * time.Second)
-
+	print(pk)
 }
